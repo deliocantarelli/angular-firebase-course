487995cbad9e8c9e75e28c5b253808de29b5e7cb
Délio Cantarelli <delio@manifestohut.com.br>
2018-04-25 10:53:44 -0300

    adding firebase dependency and some firebase test code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {initializeApp, database} from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor() {
+
+    // Initialize Firebase
+    const config = {
+      apiKey: 'AIzaSyBrQxxFXEkQwawNf6k5TzI83-kK9uCVPtY',
+      authDomain: 'angular-firebase-course-d3317.firebaseapp.com',
+      databaseURL: 'https://angular-firebase-course-d3317.firebaseio.com',
+      projectId: 'angular-firebase-course-d3317',
+      storageBucket: 'angular-firebase-course-d3317.appspot.com',
+      messagingSenderId: '815975254457'
+    };
+    initializeApp(config);
+
+    const root = database().ref();
+    root.on('value', (snapshot) => {
+      console.log(snapshot.val());
+    });
+  }
 }
