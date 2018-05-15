8f6530a04ba8b39571763d2147ba808ff4f18e75
Délio Cantarelli <delio@manifestohut.com.br>
2018-05-15 12:06:23 -0300

    added angular-firebase dependencies and tested new angularfire2 v5 with angular v6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {initializeApp, database} from 'firebase';
+import { AngularFireDatabase } from 'angularfire2/database';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,8 @@ import {initializeApp, database} from 'firebase';
 export class AppComponent {
   title = 'app';
 
-  constructor() {
+  constructor(private db: AngularFireDatabase) {
 
+    db.list<Object>('items').valueChanges().subscribe(console.log);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {AngularFireModule} from 'angularfire2';
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+import {AngularFireAuthModule} from 'angularfire2/auth';
+import { Firebase } from '../environments/firebase.config';
 
 
 import { AppComponent } from './app.component';
@@ -10,7 +14,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(Firebase.config),
+    AngularFireDatabaseModule,
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
