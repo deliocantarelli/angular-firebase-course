2c01868fac70e5d6483602e3ca9133877c198cb8
Délio Cantarelli <delio@manifestohut.com.br>
2018-05-16 11:15:12 -0300

    implementing angularfire basic operations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,5 +14,6 @@ export class AppComponent {
 
     // db.list<Object>('items').valueChanges().subscribe(console.log);
     db.object('items').valueChanges().subscribe(console.log);
+    db.object('monsters').valueChanges().subscribe(console.log);
   }
 }
--- src/app/crud/crud.component.ts
+++ src/app/crud/crud.component.ts
@@ -20,11 +20,32 @@ export class CrudComponent implements OnInit {
   }
   listPush() {
     // this.db.list('items').push({name: 'horse hoof', type: 'collectable', value: 45});
+    // this.db.list('monsters').push({name: 'centaur', strength: 10, hp: 200});
   }
-  listRemove() {}
-  listUpdate() {}
-  objUpdate() {}
+  listRemove() {
+    // this.db.list('items').snapshotChanges().subscribe(result => {
+    //   console.log(result[9].payload.val());
+    //   const toRemove = result[9].payload.ref;
+    //   this.db.list('items').remove(toRemove);
+    // });
+  }
+  listUpdate() {
+    // this.db.list('items').snapshotChanges().subscribe(result => {
+    //   const toRemove = result[9].payload.ref;
+    //   this.db.list('items').update(toRemove, {value: 100});
+    // });
+  }
+  objUpdate() {
+    this.db.object('monsters/-LCdGc1dYc2t6gTDCcLC').update({
+      strength: 35
+    });
+  }
   objSet() {
-    this.db.list('items').push({name: 'centaur', strength: 10, hp: 200});
+    this.db.object('monsters/-LCdGc1dYc2t6gTDCcLC').update({
+      strength: 35
+    });
+  }
+  objRemove() {
+    this.db.object('monsters/-LCdGc1dYc2t6gTDCcLC').remove();
   }
 }
